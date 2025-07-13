2edb3b203ab4b5290774c245ba68f08d000c7e46
Dan Gazineu <danielgazineu@gmail.com>
2025-07-13 15:27:36 -0400

    feat(config): add basic configuration validation (#54)
    This commit introduces a `config` package and a `validate` command to
    perform basic validation of `tako.yml` files.
    
    The `config.Load` function now checks for the presence of the required
    `version` and `dependents` fields. The new `tako validate` command
    allows users to easily check their configuration files for these basic
    requirements.
    
    This change also includes unit tests for the `config` package, with
    90.9% test coverage.
    
    Fixes: #14

---
+++ cmd/tako/internal/validate.go
@@ -0,0 +1,27 @@
+package internal
+
+import (
+	"fmt"
+
+	"github.com/dangazineu/tako/internal/config"
+	"github.com/spf13/cobra"
+)
+
+func init() {
+	rootCmd.AddCommand(validateCmd)
+}
+
+var validateCmd = &cobra.Command{
+	Use:   "validate [file]",
+	Short: "Validate a tako.yml file",
+	Long:  `Validate a tako.yml file.`,
+	Args:  cobra.ExactArgs(1),
+	RunE: func(cmd *cobra.Command, args []string) error {
+		_, err := config.Load(args[0])
+		if err != nil {
+			return err
+		}
+		fmt.Println("Validation successful!")
+		return nil
+	},
+}
